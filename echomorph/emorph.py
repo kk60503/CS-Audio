0366532b16d426be72a453f6017b5290cb132e26
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-02-27 10:43:21 -0800

    Emorph program done?

--- echomorph/emorph.py
+++ echomorph/emorph.py
@@ -4,7 +4,6 @@ from scipy import signal
 import scipy.io.wavfile
 import sounddevice as sd
 import numpy as np
-import librosa
 import wave
 import struct
 
@@ -17,22 +16,30 @@ def mix(s1, s2):
     return ((0.8 * s1) + (0.2 * s2))
 
 def echomorph(input):
+
     A = np.min(input)
     B = np.max(input)
-    amped = input * 2.7
-    peaks, _ = signal.find_peaks(amped)
-    print(peaks)
+    print(A)
+    print(B)
+    check = A + B
+    if check > 0:
+        peak = B
+    if check < 0:
+        peak = A
+    multiplier = 2 - peak
+    print(multiplier)
+
     clipped = np.clip(input, (A*0.4), (B*0.4))
-    sos = signal.iirfilter(4, 0.03, btype='highpass', ftype='butter', output='sos')
+    #scipy.io.wavfile.write(("clipped.wav"), sample_rate, clipped.astype(np.float32))
+    sos = signal.iirfilter(4, 0.0333, btype='highpass', ftype='butter', output='sos')
     filtered = signal.sosfilt(sos, clipped)
-    #num, den = signal.normalize(filtered, B)
-    mixed = mix(filtered, input)
-    #result = mixed * 0.7
+    amped = filtered * multiplier
+    #scipy.io.wavfile.write(("amped.wav"), sample_rate, amped.astype(np.float32))
+    mixed = mix(amped, input)
+    result = mixed * 0.7
     
-    return mixed
+    return result
 
-
-#y, sample_rate = librosa.load(infile)
 
 with wave.open(infile, "rb") as w:
     assert w.getnchannels() == 1
@@ -41,12 +48,14 @@ with wave.open(infile, "rb") as w:
     frames = w.readframes(nframes)
     framedata = struct.unpack(f"<{nframes}h", frames)
     samples = [s / (1 << 15) for s in framedata]
-    params = w.getparams()
+    #params = w.getparams()
     sample_rate = w.getframerate()
 
 result = np.array(samples)
 
+sd.play(data=(result.astype(np.float32)), samplerate=sample_rate, blocking=True)
+
 for i in range(9):
     result = echomorph(result)
     sd.play(data=(result.astype(np.float32)), samplerate=sample_rate, blocking=True)
-    scipy.io.wavfile.write(("test" + str(i) + ".wav"), sample_rate, result.astype(np.float32))
+    #scipy.io.wavfile.write(("test" + str(i) + ".wav"), sample_rate, result.astype(np.float32))
