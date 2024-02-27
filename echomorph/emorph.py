761172d5a11aac360c30c57cb72b4f6ce3d7755b
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-02-27 10:52:56 -0800

    bloop

--- echomorph/emorph.py
+++ echomorph/emorph.py
@@ -16,26 +16,31 @@ def mix(s1, s2):
     return ((0.8 * s1) + (0.2 * s2))
 
 def echomorph(input):
-
     A = np.min(input)
     B = np.max(input)
-    print(A)
-    print(B)
     check = A + B
     if check > 0:
         peak = B
     if check < 0:
         peak = A
-    multiplier = 2 - peak
-    print(multiplier)
 
+    multiplier = 2 - peak
+    print(A)
+    print(B)
+    print(multiplier)
+    print('\n')
+    
     clipped = np.clip(input, (A*0.4), (B*0.4))
     #scipy.io.wavfile.write(("clipped.wav"), sample_rate, clipped.astype(np.float32))
+    
     sos = signal.iirfilter(4, 0.0333, btype='highpass', ftype='butter', output='sos')
     filtered = signal.sosfilt(sos, clipped)
+    
     amped = filtered * multiplier
     #scipy.io.wavfile.write(("amped.wav"), sample_rate, amped.astype(np.float32))
+    
     mixed = mix(amped, input)
+    
     result = mixed * 0.7
     
     return result
@@ -48,7 +53,6 @@ with wave.open(infile, "rb") as w:
     frames = w.readframes(nframes)
     framedata = struct.unpack(f"<{nframes}h", frames)
     samples = [s / (1 << 15) for s in framedata]
-    #params = w.getparams()
     sample_rate = w.getframerate()
 
 result = np.array(samples)
