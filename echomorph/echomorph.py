61631d057b6925a7898fd340b9a5ca6c4200859e
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-03-06 14:21:46 -0800

    Update echomorph.py

--- echomorph/echomorph.py
+++ echomorph/echomorph.py
@@ -10,7 +10,7 @@ import wave
 import struct
 
 infile = './voice-note.wav'
-
+'''pseduo-random number generator'''
 rng = np.random.default_rng()
 
 
@@ -26,12 +26,13 @@ def read_wav(infile):
         return (samples, sample_rate)
     
 
+'''normalize signal amplitude'''
 def normalize(samples, peak):
     multiplier = (1/peak)
     normald = samples * multiplier
     return normald
 
-
+'''mix altered signal with previous'''
 def mix(s1, s2):
     return ((0.8 * s1) + (0.2 * s2))
 
@@ -43,12 +44,12 @@ def echomorph(input):
     A = np.min(input)
     B = np.max(input)
     clipped = np.clip(normald, (A*0.4), (B*0.4))
-    '''Kinda like using min/max instead of abs peak, ears could be biased'''
+    '''Kinda like using min/max instead of abs peak for clipping distortion; ears could be biased'''
     #clipped = np.clip(normald, (-0.4*peak), (0.4*peak))
     
     '''Original filter that I found not as harsh'''
     #sos = signal.iirfilter(4, 0.03, btype='highpass', ftype='butter', output='sos')
-
+    '''create and apply high-pass filter'''
     sos = signal.iirfilter(8, 0.085, btype='highpass', ftype='butter', output='sos')
     filtered = signal.sosfilt(sos, clipped)
 
@@ -69,7 +70,7 @@ wav_file = result
 
 for i in range(9):
     result = echomorph(result)
-    '''PLay mutated .wav file'''
+    '''Play mutated .wav file'''
     #sd.play(data=(result.astype(np.float32)), samplerate=sample_rate, blocking=True)
     wav_file = np.append(wav_file, result)
 
